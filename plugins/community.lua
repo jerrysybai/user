f1f5083e34b2ce845f3d839841005889df3af396
jerrybai <jerry.sy.bai@gmail.com>
2023-08-20 16:04:55 +0800

    use community pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,7 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.python"},
+  { import = "astrocommunity.pack.bash"},
+  { import = "astrocommunity.pack.yaml"},
 }
